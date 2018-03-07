911fbd9353b03df8ffbeb3a7ee69a8911ec582ea
Thierry1 <floveandroid@sina.com>
2018-03-07 16:37:45 +0800

    本地仓库提交了啊啊啊的注释

--- app/src/main/java/com/example/t/myapplication6/MainActivity.java
+++ app/src/main/java/com/example/t/myapplication6/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.t.myapplication6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//新添加一行代码是 啊啊啊啊啊啊啊啊啊啊啊
+//新添加一行代码是 啊啊啊啊啊啊啊啊啊啊啊啊
 public class MainActivity extends AppCompatActivity {
 
     @Override
