e22dee495c1356aa778f6c2fddeaf4d10f26308f
Thierry1 <floveandroid@sina.com>
2018-03-07 16:36:07 +0800

    添加了呵呵呵注释

--- app/src/main/java/com/example/t/myapplication6/MainActivity.java
+++ app/src/main/java/com/example/t/myapplication6/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.t.myapplication6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//新添加一行代码 嘎嘎嘎嘎嘎嘎嘎嘎嘎嘎
+//新添加一行代码 嘎嘎嘎嘎嘎嘎嘎嘎嘎嘎呵呵哈哈哈
 public class MainActivity extends AppCompatActivity {
 
+    //呵呵呵呵
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
